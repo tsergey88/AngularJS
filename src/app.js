186b8100a3cd9ccc0fbb0fb3792c13c153054c05
Sergei Timushev <tsergeyrol08@gmail.com>
2018-08-17 09:49:00 +0300

    some edit

--- src/app.js
+++ src/app.js
@@ -12,3 +12,33 @@ angular
       confirm('Долго еще? ))');
     }
   })
+  .directive('container', function() {
+    return {
+      restrict: 'EA',
+      controllerAs: 'container',
+      controller: function () {
+        this.showAlert = function () {
+          alert ('Первая кнопочка для Макара ))');
+        };
+        this.showPrompt = function () {
+          let answer = prompt('Любишь издеваться над бедными студентами? ))');
+          alert ('твой ответ ' + answer);
+        }
+        this.showConfirm = function () {
+          confirm('Долго еще? ))');
+        }
+      },
+    }
+  })
+  .directive('myButton', function() {
+    return {
+      restrict: 'EA',
+      scope: {
+        title: "@",
+      },
+      replace: true,
+      template: [
+        '<button>{{title}}</button>'
+      ].join(''),
+    }
+  })
